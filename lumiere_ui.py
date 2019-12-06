dcf837212154cbf252e7acdf32d1560cdb7cbb24
Cédric BRANDIN <clarkx@orange.fr>
2019-12-06 22:45:23 +0100

    Revert fix

--- lumiere_ui.py
+++ lumiere_ui.py
@@ -210,10 +210,9 @@ def update_spherical_coordinate(self, context, light=None):
 
 	light.location = cartesian_coordinates(r, theta, phi, light.Lumiere.hit)
 	
-	if context.scene.is_running :
-		track  = light.location - Vector(light.Lumiere.hit)
-		rotaxis = (track.to_track_quat('Z','Y'))
-		light.rotation_euler = rotaxis.to_euler()
+	track  = light.location - Vector(light.Lumiere.hit)
+	rotaxis = (track.to_track_quat('Z','Y'))
+	light.rotation_euler = rotaxis.to_euler()
 
 	# Update direction for range update
 	light.Lumiere.direction = light.matrix_world.to_quaternion() @ Vector((0.0, 0.0, 1.0))
